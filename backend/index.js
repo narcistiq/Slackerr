863c75274f2b6b1e8a977c6784eed5d8afa01e11
tiffh <tiffhuang321@gmail.com>
2025-10-11 01:28:06 -0400

    feat: init express server, mongodb connection
    Connected mongodb, used dotenv instead of the config.js file though

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@
 
 
 //this is calling all the libraries and APIs we will be using
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 
 // Import your MongoDB connection URI from the config file.
-const { MONGODB_URI } = require('./config');
+const MONGODB_URI = process.env.MONGO_URI; // from env file
 const app = express();
 
 // Import the GraphQL schema definitions and resolvers.
@@ -19,7 +20,14 @@ const resolvers = require('./graphql/resolvers');
 
 // Initialize the Express and Apollo Server instances.
 // Then start the server with a function
-
+mongoose.connect( MONGODB_URI );
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+})
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 async function startApolloServer() {
     await server.start();
     server.applyMiddleware({ app });
