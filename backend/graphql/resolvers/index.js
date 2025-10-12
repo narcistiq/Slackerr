4c54e2bfc6b05f42fc81d60714df58e777d089b3
tiffh <tiffhuang321@gmail.com>
2025-10-11 21:58:24 -0400

    feat: merge resolvers

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -3,27 +3,20 @@
 
 // rename it according to whatever types you are using!
 
-/* 
 
 // here go each of the different data types:
-
-const artistResolvers = require('./artist');
 const userResolvers = require('./user');
-const postResolvers = require('./post');
+const applicationResolvers = require('./application');
 
 
 module.exports = {
 
-
     Query: {
-        ...artistResolvers.Query,
         ...userResolvers.Query,
-        ...postResolvers.Query
+        ...applicationResolvers.Query
     },
     Mutation: {
-        ...artistResolvers.Mutation,
         ...userResolvers.Mutation,
-        ...postResolvers.Mutation
+        ...applicationResolvers.Mutation
     }
 };
-*/
