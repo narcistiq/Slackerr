bd8cb64e17193888ded65c73c195eb531698d4b1
tiffh <tiffhuang321@gmail.com>
2025-10-13 00:36:43 -0400

    feat: basic menu layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { useState } from "react";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   HttpLink,
+//   ApolloLink,
+// } from '@apollo/client';
+// import { ErrorLink } from '@apollo/client/link/error'
+
+// const link = ApolloLink.from([
+//   ErrorLink,
+//   new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+// ]);
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: link
+// })
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  const [input, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='title'>
+      <h2>Slackerr</h2>
+    </div>
+    <div className='login'>
+      <form>
+        <input type="email" value={input} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+        <button type="submit">Login</button>
+        <button>Sign Up</button>
+      </form>
+    </div>
     </>
-  )
+  ); 
 }
-
-export default App
+export default App;
