a68d20324fb8610737e44e3230c9ec3239a8701c
tiffh <tiffhuang321@gmail.com>
2025-10-16 01:16:57 -0400

    feat: stylize application log page

--- frontend/src/components/Log.jsx
+++ frontend/src/components/Log.jsx
@@ -2,7 +2,29 @@ import "./Log.css"
 
 function Log() {
     return (
-        <></>
+        <>
+        <div className="log">
+            <h1>Slackerr</h1>
+            <div className="wrapper">
+                <div className="boxes">
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <input className="myInput"/>
+                    <button className="add">Add</button>
+                </div>
+                <div className="applications-box">
+                    <ul>
+                        <li>list</li>
+                        <li>another item</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
 
