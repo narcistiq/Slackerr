1b675ccf03e449201699e85e36da09d4dc6e6908
tiffh <tiffhuang321@gmail.com>
2025-10-21 21:57:17 -0400

    style: sign up page

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,13 +1,14 @@
 import "./Login.css"
 
 function SignUp() {
+    
     return (
         <>
         <form>
-            <input className="input" type="email" placeholder="Email" required />
-            <input className="input" type="password" required/>
-              <button type="submit" className="btn">Login</button>
-              <button className="btn signup">Sign Up</button>
+            <input className="input signup" type="email" placeholder="Email" required />
+            <input className="input signup" type="password" placeholder="Password" required/>
+            <input className="input signup" type="password" placeholder="Re-type Password" required/>
+            <button className="btn signup-page">Sign Up</button>
         </form>
         </>
     );
